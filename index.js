9f88c88464c168569f9d5de4aaccebeb1ff526a9
Ravikumar7676 <57000379+Ravikumar7676@users.noreply.github.com>
2023-01-11 13:49:39 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let i,
   a = 100;
-let count = 0;
-console.log('count of All odd numbers from 1 to 100');
 for (i = 1; i <= a; i++) {
-  if (i % 3 == 0) {
+  if (i % 5 == 0 && i % 3 == 0) {
+    console.log('amazon');
+  } else if (i % 3 == 0) {
     console.log('google');
+  } else if (i % 5 == 0) {
+    console.log('facebook');
   }
 }
-console.log(count);
